4a119890ab002840ea02dd7362b95ce9b220e550
David Migoya Hernández <25davex@gmail.com>
2023-04-03 10:26:19 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import org.apache.tika.gui.TikaGUI;
+
+public class Main {
+
+    //"SIPATI" (Sufix and PATricia Tree Indexer)
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+    }
+}
